0e528f3adceba5e687d72b05bf170f1ea033fbb4
renle <renle@zto.com>
2020-01-20 15:52:42 +0800

    git init

---
+++ server.js
@@ -0,0 +1,17 @@
+let http = require("http");
+let users = [
+  { id: 1, name: "zhufenf1" },
+  { id: 2, name: "zhufeng2" },
+  { id: 3, name: "zhufeng3" }
+];
+let server = http.createServer(function(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  if (req.url === "/api/users") {
+    res.end(JSON.stringify(users));
+  } else {
+    res.end("Not Found");
+  }
+});
+server.listen(3000, () => {
+  console.log("后端api接口服务启动");
+});
